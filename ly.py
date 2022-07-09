477dd4e312b04e7d0f51cfae9a7092eea65956c2
Christina Hanson <christinahansondesu@gmail.com>
2022-07-09 13:54:28 +1200

    Fix bracket matching in char literals

--- ly.py
+++ ly.py
@@ -59,7 +59,7 @@ except FileNotFoundError:
 
 # remove comments and strings
 uncommented_program = re.sub(re.compile(
-    '"(?:\\\\.|[^"\\\\])*"', re.DOTALL | re.MULTILINE), "", program)
+    r'"(?:\\.|[^"\\])*"|\'.', re.DOTALL | re.MULTILINE), "", program)
 
 uncommented_program = re.sub(re.compile("#(.*)"), "", uncommented_program)
 
